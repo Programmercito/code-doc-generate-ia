f208b3fbe65b235ca782e990a02ecf7f647b03ca
joa <joa123456789333@mail.com>
2025-03-01 15:11:15 -0400

    Add functionality to send Java file content to specified URL and handle responses

--- java-doc.py
+++ java-doc.py
@@ -1,8 +1,10 @@
 # imprimo start script
 import os
+import requests
 from dotenv import load_dotenv, find_dotenv
 import fnmatch
 import re
+import time
 
 # Verificar si el archivo .env existe y cargar las variables de entorno
 dotenv_path = find_dotenv()
@@ -17,8 +19,11 @@ print("start script")
 # obtengo la ruta de el .env con el parametro ruta
 ruta = os.getenv("ruta")
 prompt = os.getenv("prompt")
+url = os.getenv("url")
+model = os.getenv("model")
 
 print(f"ruta a documentar: {ruta}")
+
 
 def obtener_archivos_java(ruta, prompt):
     archivos = []
@@ -32,9 +37,21 @@ def obtener_archivos_java(ruta, prompt):
                 archivos.append({"ruta": file_path, "contenido": con})
     return archivos
 
+
 archivos_java = obtener_archivos_java(ruta, prompt)
+
+
+def send_ollama(url, archivo_java):
+    enviar = {"model": model, "prompt": archivo_java,"stream": False}
+    response = requests.post(url, json=enviar)
+    return response
+
 
 # Imprimir los resultados
 for archivo in archivos_java:
     print(f"Ruta: {archivo['ruta']}")
     print(f"Contenido:\n{archivo['contenido']}\n")
+    response = send_ollama(url, archivo["contenido"])
+    print(f"Respuesta: {response.text}")
+    time.sleep(15)
+    
